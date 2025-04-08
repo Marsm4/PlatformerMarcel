61ed7aa379e000809d38bc10dc4231dc4f79624f
Мубаракшин Марсель <marselmubaraksin231@mail.ru>
2025-04-08 23:14:46 +0300

    мелкие правки

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -7,11 +7,31 @@ public class Coin : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        // Более надежная проверка тега
+        if (!other.CompareTag("Player")) return;
+
+        // Проверка GameManager
+        if (GameManager.Instance == null)
         {
-            GameManager.Instance.AddScore(scoreValue);
-            AudioSource.PlayClipAtPoint(collectSound, transform.position);
-            Destroy(gameObject);
+            Debug.LogError("GameManager not found!");
+            return;
         }
+
+        // Добавление очков
+        GameManager.Instance.AddScore(scoreValue);
+
+        // Воспроизведение звука
+        if (collectSound != null)
+            AudioSource.PlayClipAtPoint(collectSound, transform.position);
+
+        // Уничтожение монеты
+        Destroy(gameObject);
+    }
+
+    // Визуализация триггера в редакторе
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, GetComponent<CircleCollider2D>().radius);
     }
 }
